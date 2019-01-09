84bc380166039ae4fdbdd986359f81c03817027a
pyc2238 <pyc2238@naver.com>
2019-01-09 15:07:21 +0900

    Code theorem

--- resources/views/home/component/footer.blade.php
+++ resources/views/home/component/footer.blade.php
@@ -1,22 +1,31 @@
+<style>
+    div>.copyright>a{
+        color:#00e0d0;
+    }
+
+</style>
+
+
 <footer class="footer" style='margin-top:1%'>
     <div class="col Internationalization">
-        <a href=""><img src="{{asset('data\ProjectImages\master/korea.png')}}" alt="Korea">&nbsp;Korean&nbsp;&nbsp;</a>
+        <a href=""><img src="{{asset('data\ProjectImages\master/korea.png')}}" alt="Korea">&nbsp;&nbsp;Korean&nbsp;&nbsp;</a>
         <a href=""><img src="{{asset('data\ProjectImages\master/japan.png')}}" alt="Japan">&nbsp;&nbsp;Japanese&nbsp;&nbsp;</a>
 
     </div>
     <div class="w-100"></div>
     <div class="col copyright">
         <div class="row">
-            <div class="col-md12" style="margin-left:3%;">
-                <a href="{{route('home.index')}}"><img src="{{asset("data/ProjectImages/master/NEITTER.png")}}" alt="Logo"
+            <div class="col-md12" style="margin-left:3%;margin-top:1%">
+                <a href="{{route('home.index')}}"><img src="{{asset("data/ProjectImages/master/footerLog.png")}}" alt="Logo"
                         ondragstart="return false"></a>
             </div>
-            <div class="col-md4" style="margin-left:3%;">
-                <a href="{{route('home.introduction')}}">NEITTER소개</a>&nbsp;|&nbsp;
-                <a href="#">이용약관</a>&nbsp;|&nbsp;
-                <a href="#">개인정보취급방침</a>&nbsp;|&nbsp;
-                <a href="#">버그 및 불편사항 신고</a>&nbsp;|&nbsp;
-                <a href="mailto:pyc2238@gmail.com">운영자 문의</a>
+            <div class="col-md4 copyright" style="margin-left:3%;">
+                <a href="{{route('home.introduction')}}"><b>NEITTER소개</b></a>&nbsp; |
+                <a href="#"><b>이용약관</b></a>&nbsp; | 
+                <a href="#"><b>개인정보취급방침</b></a>&nbsp; | 
+                <a href="#"><b>버그 및 불편사항 신고</b></a>&nbsp; | 
+                <a href="mailto:pyc2238@gmail.com"><b>운영자 문의</b></a>&nbsp; | &nbsp;
+                <a href=""><img src="{{asset("data/ProjectImages/master/escort.png")}}" alt="escort"></a>
                 <br>
                 <br>
                 서비스 운영<br>
@@ -31,7 +40,7 @@
                 <br>
                 Copyright © Gratefuldays Corp. All rights reserved.<br>
                 Copyright © Joyandnetworks Corp. All rights reserved.<br>
-                Copyright © Korea Japan Culture Exchange KJLUB All rights reserved<br>
+                Copyright © Korea Japan Culture Exchange NEITTER All rights reserved<br>
             </div>
         </div>
 
