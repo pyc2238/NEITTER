3ca23a0ac83a02baa858298e09f6ca7738eb11bc
pyc2238 <pyc2238@naver.com>
2019-01-16 09:32:28 +0900

    Regionalization of error message

---
+++ resources/lang/ko/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '아이디또는 비밀번호가 일치하지 않습니다.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
