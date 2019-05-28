5efd482f87845648170990be7586bd7a9f3ea780
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-05-28 19:58:00 +0900

    add penpal list search3

--- app/Http/Controllers/Penpal/ViewController.php
+++ app/Http/Controllers/Penpal/ViewController.php
@@ -22,6 +22,7 @@ class ViewController extends Controller
   
 
     public function __construct(){
+        
         $this->timelineModel    = new Timeline();
         $this->penpalModel      = new Penpal();
         $this->userModel        = new User();
@@ -33,90 +34,72 @@ class ViewController extends Controller
     //펜팔 메인 페이지
     public function index (Request $request){
       
-        //default value
-        $penpals =  $this->penpalModel->getUsers()->latest()->paginate(12);
+        //base Object
+        $penpals =  $this->penpalModel->getUsers();
         
 
-        //닉네임 검색시 
-        if($request->name){
-    
-            $user = $this->userModel->where('name',$request->name)->first();
+        //닉네임 검색
+        if (!empty($request->name)) {
+            $users = $this->userModel->where('name', 'like', '%' . $request->name . '%')->get();
+            if (!empty($users)) {
 
-            //검색 결과가 없을 시 
-            if(!$user){
-                return back()->with('message','해당 닉네임의 검색결과가 없습니다.');
-            }
+                $penpals->whereIn('user_id', $users);
+            }        
+        }
 
-            foreach($penpals as $penpal){
-                $penpals = $penpal->where('user_id',$user->id)->latest()->paginate(12);
-            }          
-           
+
+        //성별 검색
+        if (!empty($request->gender) && $request->gender !== 'all') { 
+            $penpals->leftJoin('users', 'penpals.user_id', '=', 'users.id')
+            ->select('penpals.*', 'users.gender')
+            ->where('users.gender', $request->gender); 
+        }
+
+
+        // 국적 검색 
+        if (!empty($request->country) && $request->country !== 'all') { 
+        $penpals->leftJoin('users', 'penpals.user_id', '=', 'users.id')
+        ->select('penpals.*', 'users.country')
+        ->where('users.country', $request->country); 
+        }
+
+
+         // 목적 검색
+         if (!empty($request->goal) && $request->goal !== 'all') {
+ 
+            $penpals = $this->penpalModel->where('goal_id',$request->goal)->latest();
          }
 
 
-            //성별 검색 
-            if($request->gender != null){
-                if($request->gender != 'all'){
-                    $penpals = $this->penpalModel->leftJoin('users', 'penpals.user_id', '=', 'users.id')
-                    ->select('penpals.*', 'users.gender')
-                    ->where('users.gender', $request->gender)
-                    ->orderBy('penpals.created_at','desc')->paginate(12);
-                }
-            }
-            
+        //나이로 검색
+        if($request->ageMin != 1 || $request->ageMax != 100 ){
 
-            // 국적 검색 
-            if($request->country != null){
-                if($request->country != 'all'){
-                    $penpals = $this->penpalModel->leftJoin('users', 'penpals.user_id', '=', 'users.id')
-                    ->select('penpals.*', 'users.country')
-                    ->where('users.country', $request->country)
-                    ->orderBy('penpals.created_at','desc')->paginate(12);
-                }
-             }
+            $ageMin = floor($request->ageMin);
+            $ageMax = floor($request->ageMax);
 
-            // 목적 검색
-             if($request->goal != null){
-                if($request->goal != 'all'){
-                    $penpals = $this->penpalModel->where('goal_id',$request->goal)->latest()->paginate(12);
+            $penpals = $this->penpalModel->leftJoin('users', 'penpals.user_id', '=', 'users.id')
+            ->select('penpals.*', 'users.age')
+            ->whereBetween('users.age', [$ageMin, $ageMax])
+            ->orderBy('penpals.created_at','desc');
 
-                    if(!$penpals){
-                        return back()->with('message','해당 닉네임의 검색결과가 없습니다.');
-                    }
-                }
-            }
+        }
 
-            //나이로 검색
-            if($request->ageMin != 1 || $request->ageMax != 100 ){
-    
-                $ageMin = floor($request->ageMin);
-                $ageMax = floor($request->ageMax);
-
-                $penpals = $this->penpalModel->leftJoin('users', 'penpals.user_id', '=', 'users.id')
-                ->select('penpals.*', 'users.age')
-                ->whereBetween('users.age', [$ageMin, $ageMax])
-                ->orderBy('penpals.created_at','desc')->paginate(12);
-
-                if(!$penpals){
-                    return back()->with('message','해당 연령대의 검색결과가 없습니다.');
-                }
-
-            } 
-
-            
-         //내용 번역
-         foreach($penpals as $penpal){
+        
+        //search result
+        $penpalsData = $penpals->orderBy('penpals.created_at','desc')->paginate(12); 
+         
+        //내용 번역
+         foreach($penpalsData as $penpal){
 
             $translationTimeline = $this->translation($penpal->self_context,$this->langCode($penpal->self_context));
             $penpal->translation = $translationTimeline;
          
         }
         
-      
-        $penpalsCount = count($penpals);        //검색 조회 펜팔 결과 수
+        $penpalsCount = count($penpalsData);
 
         return view('penpal.index')->with([
-            'penpals'       => $penpals,
+            'penpals'       => $penpalsData,
             'penpalsCount'  => $penpalsCount
             ]);
 
