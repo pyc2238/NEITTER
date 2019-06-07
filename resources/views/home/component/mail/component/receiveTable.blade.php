edac1951bb86fd8b125887ff16c5058b68f91fe1
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-06-07 10:25:25 +0900

    add mail form

---
+++ resources/views/home/component/mail/component/receiveTable.blade.php
@@ -0,0 +1,28 @@
+<div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col"><input type="checkbox" id="checkall" /></th>
+                        <th scope="col">보낸이</th>
+                        <th scope="col">내용</th>
+                        <th scope="col">수신시간</th>
+                        <th scope="col">삭제</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"><input type="checkbox" name="chk" /></th>
+                        <td>관리자</td>
+                        <td>안녕하세요</td>
+                        <td>18-07-18 01:54</td>
+                        <td><button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="col">
+            <button class="btn btn-danger" type="button">선택삭제</button>
+        </div>
