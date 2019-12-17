c757aa33630dce7288fe6746da3a6958eae01727
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-12-17 22:24:28 +0900

    add umr site

--- public/js/umr.js
+++ public/js/umr.js
@@ -2,6 +2,6 @@ function showTitle() {
     $("#showUmrBox").show();
     setTimeout(function() {
         $("#showUmrBox").hide();
-        location.href='ewfeffdsf';
+        location.href='https://namu.wiki/w/%EB%8F%84%EB%A7%88%20%EC%9A%B0%EB%A7%88%EB%A3%A8';
     },6000);
 }
